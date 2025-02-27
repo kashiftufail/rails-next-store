22ef9d4a1a69601d4dc703083a0cec4f814c6bac
amir <amir@codility.co>
2025-02-27 20:37:36 +0500

    change in products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,24 @@
 class Api::V1::ProductsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-        # binding.pry
-        # ffffffff
-    posts = Product.all
-    render json: posts
-  end
+
+    def index
+      products1 = Product.all
+      # products = Product.includes(:images).all
+  
+      # Prepare the response by attaching image URLs to each product
+      products = products1.map do |product|
+        {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          # Generate full URLs for all images attached to this product
+          images: product.images.map { |image| rails_blob_url(image) }
+        }
+      end
+       
+      # Render products with their images
+      render json: products
+    end
+  
 end
